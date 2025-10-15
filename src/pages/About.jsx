62e5b58e868497462339e99b73569284ea54d240
Nahom Hul <nahomhulum@gmail.com>
2025-10-15 14:29:06 +0300

    contact and about pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>FindRecipe is a Capstone project designed to make cooking fun and easy.</p>
+    </div>
+  );
+}
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Reach us at findrecipe@foodzone.io</p>
+    </div>
+  );
+}
