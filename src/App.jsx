25fcfc1098ace6ba615d51faa4c6f85e49b9fb01
Nahom Hul <nahomhulum@gmail.com>
2025-10-15 11:32:56 +0300

    import home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import SearchBox from "./components/SearchBox";
 import RecipeTile from "./components/RecipeTile";
+import { fetchRecipes } from "./services/recipeApi";
 
 export default function App() {
   const [recipeList, setRecipeList] = useState([]);
+  const [featuredRecipes, setFeaturedRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   return (
     <div>
@@ -11,6 +14,12 @@ export default function App() {
       {recipeList.map((r) => (
         <RecipeTile key={r.idMeal} recipeInfo={r} />
       ))}
+      <div>
+        <h3>Featured Recipes</h3>
+        {featuredRecipes.map((r) => (
+          <RecipeTile key={r.idMeal} recipeInfo={r} />
+        ))}
+      </div>
     </div>
   );
 }
